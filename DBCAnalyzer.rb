95b140b4f81d20663a028ddda924fc7c15b6f253
Even <liyiwen007@gmail.com>
2017-08-06 14:10:05 +0800

    Change tab to space

--- DBCAnalyzer.rb
+++ DBCAnalyzer.rb
@@ -1,78 +1,78 @@
 require "erb" 
 
 class CANSignal
-	attr_accessor :name, :start_bit, :signal_size, :is_big_endian, :is_unsigned
-	def initialize(name = "", start_bit = 0, signal_size = 0, is_big_endian = false, is_unsigned = false)
-		@name, @start_bit, @signal_size = name, start_bit, signal_size
-		@is_big_endian, @is_unsigned = is_big_endian, is_unsigned
-	end
+    attr_accessor :name, :start_bit, :signal_size, :is_big_endian, :is_unsigned
+    def initialize(name = "", start_bit = 0, signal_size = 0, is_big_endian = false, is_unsigned = false)
+        @name, @start_bit, @signal_size = name, start_bit, signal_size
+        @is_big_endian, @is_unsigned = is_big_endian, is_unsigned
+    end
 end
 
 class CANMessage
-	attr_accessor :name, :id, :signals
+    attr_accessor :name, :id, :signals
 
-	def initialize(name = "", id = 0) 
-		@name, @id = name, id
-		@signals = []
-	end
+    def initialize(name = "", id = 0) 
+        @name, @id = name, id
+        @signals = []
+    end
 
 end
 
 class DBCFileDescriptor
-	attr_accessor :messages
+    attr_accessor :messages
 
-	def initialize() 
-		@messages = []
-	end
+    def initialize() 
+        @messages = []
+    end
 end
 
 class CAN_DBC_Analzyer
-	
-	def AnalyzeDBCFile(file_name)
-		file_descriptor = DBCFileDescriptor.new 
-		
-		File.open(file_name) do | dbc_file |
-			parser_partterns = CAN_DBC_Analzyer.new.methods.select{
-				 |item| item.to_s =~ %r!^DBC.+Matcher$! 
-				}.map{ |item| method(item) }
-			dbc_file.each_line do | line |
-				parser_partterns.each { | parttern | parttern.call(line, file_descriptor) }
-			end
-		end
-		return file_descriptor
-	end
+    
+    def AnalyzeDBCFile(file_name)
+        file_descriptor = DBCFileDescriptor.new 
+        
+        File.open(file_name) do | dbc_file |
+            parser_partterns = CAN_DBC_Analzyer.new.methods.select{
+                 |item| item.to_s =~ %r!^DBC.+Matcher$! 
+                }.map{ |item| method(item) }
+            dbc_file.each_line do | line |
+                parser_partterns.each { | parttern | parttern.call(line, file_descriptor) }
+            end
+        end
+        return file_descriptor
+    end
 
-	def DBCSignalMatcher(line, file_des = nil)
-		if line =~ %r%^\s*SG_\s+(\w+)\s+:\s+(\d+)\|(\d+)@(\d)(\+|-).*$% then
-			signal = CANSignal.new($1, $2.to_i, $3.to_i, $4 == "0", $5 == "+")
-			signal.start_bit = ChangeMotorolaOrderMSB2LSB($2.to_i, $3.to_i) if signal.is_big_endian
-			if file_des != nil then
-				file_des.messages.last.signals.push signal			
-			end
-			signal
-		end	
-	end
+    def DBCSignalMatcher(line, file_des = nil)
+        if line =~ %r%^\s*SG_\s+(\w+)\s+:\s+(\d+)\|(\d+)@(\d)(\+|-).*$% then
+            signal = CANSignal.new($1, $2.to_i, $3.to_i, $4 == "0", $5 == "+")
+            signal.start_bit = ChangeMotorolaOrderMSB2LSB($2.to_i, $3.to_i) if signal.is_big_endian
+            if file_des != nil then
+                file_des.messages.last.signals.push signal          
+            end
+            signal
+        end 
+    end
 
-	def DBCMessageMatcher(line, file_des = nil)
-		if line =~ %r|^BO_\s+(\d+)\s+(\w+)\s*:.*$| then
-			msg = CANMessage.new($2, $1.to_i)
-			if file_des != nil then
-				file_des.messages.push msg
-			end
-			msg
-		end
-	end
+    def DBCMessageMatcher(line, file_des = nil)
+        if line =~ %r|^BO_\s+(\d+)\s+(\w+)\s*:.*$| then
+            msg = CANMessage.new($2, $1.to_i)
+            if file_des != nil then
+                file_des.messages.push msg
+            end
+            msg
+        end
+    end
 
-	private
-	# because in DBC, if a signal is big_endian (Motorola order),
-	# DBC file uses MSB as start bit.
-	# otherwise, it uses LST as start bit.
-	def ChangeMotorolaOrderMSB2LSB(start_bit, signal_size)
-		while (signal_size -= 1) != 0 do
-			start_bit += (start_bit % 8) == 0 ? 15 : -1
-		end
-		start_bit
-	end
+    private
+    # because in DBC, if a signal is big_endian (Motorola order),
+    # DBC file uses MSB as start bit.
+    # otherwise, it uses LST as start bit.
+    def ChangeMotorolaOrderMSB2LSB(start_bit, signal_size)
+        while (signal_size -= 1) != 0 do
+            start_bit += (start_bit % 8) == 0 ? 15 : -1
+        end
+        start_bit
+    end
 end
 
 #=begin
@@ -82,8 +82,8 @@ can_database = analyzer.AnalyzeDBCFile("test.dbc")
 puts "Start Generate Code"
 template ="code_template.txt"
 File.open( template ) { |fh| 
-	erb_engine = ERB.new( fh.read ) 
-	print erb_engine.result( binding )    
+    erb_engine = ERB.new( fh.read ) 
+    print erb_engine.result( binding )    
 }
 #=end
 
