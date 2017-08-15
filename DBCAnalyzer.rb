50181efc8eb83fc353597d38b3a06efa3e21e6d1
Even <liyiwen007@gmail.com>
2017-08-15 23:03:46 +0800

    add some attribute analysis

--- DBCAnalyzer.rb
+++ DBCAnalyzer.rb
@@ -4,7 +4,7 @@ require "erb"
 
 class CANSignal
     attr_accessor :name, :start_bit, :size, :is_big_endian, :is_unsigned
-    attr_accessor :factor, :offset, :minimun, :maximun, :unit, :receivers
+    attr_accessor :factor, :offset, :minimun, :maximun, :unit, :receivers, :attributes
 
     def initialize(name = "", start_bit = 0, size = 0, is_big_endian = false, is_unsigned = false)
         @name, @start_bit, @size = name, start_bit, size
@@ -12,15 +12,17 @@ class CANSignal
         @factor, @offset, @minimun, @maximun = 0.0, 0.0, 0.0, 0.0
         @unit = ""
         @receivers = []  # store the names of receivers for this signal
+        @attributes = {}
     end
 end
 
 class CANMessage
-    attr_accessor :name, :id, :signals, :size, :transmitter
+    attr_accessor :name, :id, :signals, :size, :transmitter, :attributes
 
     def initialize(name = "", id = 0, size = 0, transmitter = "") 
         @name, @id, @size, @transmitter = name, id, size, transmitter
         @signals = []
+        @attributes = {}
     end
 
 end
@@ -55,6 +57,7 @@ class CAN_DBC_Analzyer
             signal = CANSignal.new($1, $2.to_i, $3.to_i, $4 == "0", $5 == "+")
             signal.start_bit = ChangeMotorolaOrderMSB2LSB($2.to_i, $3.to_i) if signal.is_big_endian
 
+            # [maximun|minmun] need to be updated. to support scientific notation
             if $6 =~ %r!\((-?\d+(\.\d+)?),(-?\d+(\.\d+)?)\)\s+\[.*\|.*\](.+)!
                 signal.factor = $1.to_f
                 signal.offset = $3.to_f
@@ -83,11 +86,22 @@ class CAN_DBC_Analzyer
             msg
         end
     end
-=begin
-    def DBCCommentMatcher(line, file_des = nil)
-        puts "comment"
+
+    def DBCAttributeValueMatcher(line, file_des = nil)
+        #puts "In DBCAttributeValueMatcher"
+        if line =~ %r|^BA_\s+"(\w+)"\s+(.*);$| then
+            attr_name = $1
+            #puts "Find a attribute #{attr_name}"
+            #puts "$2 is |#{$2}|"
+            case $2
+            when /^BO_\s+(\d+)\s+(\d+)$/ then
+                #puts "Find a attribute For message id: #{$1} and value is #{$2}"
+                msg = file_des.messages.find {|msg| msg.id == $1.to_i }
+                msg.attributes[attr_name.to_sym] = $2.to_i
+            end
+        end
     end
-=end
+
     private
     # because in DBC, if a signal is big_endian (Motorola order),
     # DBC file uses MSB as start bit.
@@ -109,7 +123,12 @@ if ARGV.size > 0 and ARGV[0] =~ /.*\.dbc/
     can_database = analyzer.AnalyzeDBCFile(ARGV[0])
     puts "Finished DBC file parsing"
     puts "Start Generate Code"
-    template_list = Dir["./*.erb"]
+#=begin
+    if ARGV.size == 2 and ARGV[1] =~ /.*\.erb/
+        template_list = [ARGV[1]]
+    else
+        template_list = Dir["./*.erb"]
+    end
     template_list.each do | temp_file |
         if temp_file =~ /(.*)\.erb/
             f = File.new($1, "w") 
@@ -120,4 +139,5 @@ if ARGV.size > 0 and ARGV[0] =~ /.*\.dbc/
             }
         end
     end
+#=end
 end
